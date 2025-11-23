d48d037a64c8a5fee2de98ce2dd8c41e4176e8f0
zikran11 <ziikran123@gmail.com>
2025-11-23 16:39:56 +0700

    Remove dist folder from Git tracking

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
 import Gallery from '../views/Gallery.vue'
@@ -28,7 +28,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(), // FIX UTAMA
   routes,
   scrollBehavior() {
     return { top: 0 }
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: '/eco/',   // <-- WAJIB untuk GitHub Pages
   plugins: [vue()],
   server: {
     host: true,
